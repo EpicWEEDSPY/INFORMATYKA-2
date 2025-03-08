b5b1748a3b08039b7dff6dd90710d4f830e8940d
EpicWEEDSPY <148873186+EpicWEEDSPY@users.noreply.github.com>
2025-03-08 17:41:53 +0100

    Add files via upload

---
+++ Zmienne Tablicowe/Wprowadzenie do C++/3. Typy danych.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int calk = 10;
+    float zmien = 3.14;
+    char znak = 'A';
+    bool log = true;
+    cout << "Zmienna calkowita: " << calk << endl;
+    cout << "Zmienna zmiennoprzecinkowa: " << zmien << endl;
+    cout << "Zmienna znakowa: " << znak << endl;
+    cout << "Zmienna logiczna: " << log << endl;
+    return 0;
+}
